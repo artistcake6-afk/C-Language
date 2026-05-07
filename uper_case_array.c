1d0329f8080fd4ec310d12a928faff5b838917d7
artistcake6-afk <artistcake6@gmail.com>
2026-05-07 10:45:32 +0530

    count uper latter

---
+++ uper_case_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            count++;
+        }
+    }
+
+    printf("Total Uppercase Letters = %d", count);
+
+    return 0;
+}
