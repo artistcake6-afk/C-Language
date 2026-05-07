f49ca4194935ea8997c3901ba8fadcc8b792666e
artistcake6-afk <artistcake6@gmail.com>
2026-05-07 19:53:27 +0530

    circle program

---
+++ CLOCK.C
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int no1,no2;
+	clrscr();
+	cout<<"enter no1:"<<endl;
+	cin>>no1;
+	cout<<"enter no2:"<<endl;
+	cin>>no2;
+	if(no1>no2)
+	{
+		cout<<"the larger no is:"<<no1<<endl;
+	}
+	else
+	{
+		cout<<"the larger no is:"<<no2<<endl;
+	}
+	getch();
+}
